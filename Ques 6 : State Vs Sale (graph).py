67de80bf8e58a897ec18460018a73c4131284f13
Ashish-khar <65498683+Ashish-khar@users.noreply.github.com>
2021-12-24 13:40:25 +0530

    Update Ques 6 : State Vs Sale (graph).py

--- Ques 6 : State Vs Sale (graph).py
+++ Ques 6 : State Vs Sale (graph).py
@@ -2,22 +2,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+#Loading data
 data = pd.read_csv("C:/Users/AK/Downloads/datas.csv")
 
+#Creating data frame including Physician ID and State
+new_data = pd.DataFrame(data=data[["Physician ID"," State"]])
 
+#Calculating total prescriptions
+new2 =  pd.DataFrame(data=data[["Jan 2019","Feb 2019","Mar 2019","Apr 2019", "May 2019","Jun 2019","Jul 2019", "Aug 2019","Sep 2019", "Oct 2019","Nov 2019", "Dec 2019"]]) 
+new_data["Total"] = new2.sum(axis=1)
 
-new_data = pd.DataFrame(data=data[" State"])
+#Grouping by State
+final2 = new_data.groupby(' State').sum()
 
-new2 = pd.DataFrame(data=data[["Jan 2019","Feb 2019","Mar 2019","Apr 2019", "May 2019","Jun 2019","Jul 2019", "Aug 2019","Sep 2019", "Oct 2019","Nov 2019", "Dec 2019"]])
-new2["Total"]=pd.DataFrame(data=new2.sum(axis=1))  
-totf= pd.DataFrame(data=new2["Total"])
-
-final = pd.concat([new_data,totf],axis=1)
-
-
-
-final2 = final.groupby(' State').sum()
-
+#Sorting wrt total prescriptions
 rank=final2.sort_values(by="Total",ascending=False)
 
-rank.plot.bar()
+#Visualizing using bar graph
+rank = rank.plot(y=["Total"],kind = 'bar',title='Market Share of Different Sates',figsize=(15,5))
+rank.set_xlabel(" State")
+rank.set_ylabel("Market Share of Different Sates")
